dea57674f7b27678b7b756a4140511628ad2c8e3
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-07 10:23:29 +0200

    Create script.js

---
+++ js basic/types/mapAndSet/script.js
@@ -0,0 +1,27 @@
+/*Task 1
+
+function unique(arr){
+    return Array.from(new Set(arr));
+}
+
+let values = ["Hare", "Krishna", "Hare", "Krishna",
+  "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+alert( unique(values) );
+
+*/
+
+/*Task 2
+
+let map = new Map();
+
+map.set("name", "John");
+
+let keys = Array.from(map.keys());
+
+keys.push("more");
+
+alert(keys);
+
+*/
