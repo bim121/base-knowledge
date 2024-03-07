6b163ceccdd9d862737abacb5f30da0a80485c58
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-07 18:10:40 +0200

    Create script.js

---
+++ js basic/function/named function expression/script.js
@@ -0,0 +1,41 @@
+/*Task 1
+
+function sum(a) {
+    let currentSum = a;
+
+    function f(b){
+        currentSum += b;
+        return f;
+    }
+
+    f.toString = function(){
+        return currentSum;
+    }
+
+    return f;
+}
+
+alert( sum(1)(2) ); 
+alert( sum(5)(-1)(2) ); 
+alert( sum(6)(-1)(-2)(-3) ); 
+alert( sum(0)(1)(2)(3)(4)(5) ); 
+
+*/
+
+/*Task 2
+
+function makeCounter(){
+    let count = 0;
+    
+    function counter(){
+        return count++;
+    }
+
+    counter.set = value => count = value;
+
+    counter.decrease = () => count--;
+
+    return counter;
+}
+
+*/
