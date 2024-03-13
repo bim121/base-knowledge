8f3e5f24985db8ac94b20e5d7282c49232fc9e2f
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-13 14:42:48 +0200

    Create movie.service.spec.ts

---
+++ angular basic/netflix-clone-angular/src/app/shared/services/movie.service.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, async, inject } from '@angular/core/testing';
+import { MovieService } from './movie.service';
+
+describe('Service: Movie', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [MovieService]
+    });
+  });
+
+  it('should ...', inject([MovieService], (service: MovieService) => {
+    expect(service).toBeTruthy();
+  }));
+});
