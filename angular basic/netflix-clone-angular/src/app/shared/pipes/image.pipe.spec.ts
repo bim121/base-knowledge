8039aafe9b3f5af11e4a47fe5bc9696dc77e37fd
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-13 14:44:06 +0200

    Create image.pipe.spec.ts

---
+++ angular basic/netflix-clone-angular/src/app/shared/pipes/image.pipe.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed, async } from '@angular/core/testing';
+import { ImagePipe } from './image.pipe';
+
+describe('Pipe: Imagee', () => {
+  it('create an instance', () => {
+    let pipe = new ImagePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
