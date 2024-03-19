863a1de7747fdf0d84ef285b16bcee9f8ad30a0b
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-19 18:52:18 +0200

    Create app-routing.module.ts

---
+++ angular-canvas/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { authorizedGuard } from './guards/authorized.guard';
+
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo : 'login',
+    pathMatch:'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path:'dashboard',
+    canActivate: [authorizedGuard],
+    component: DashboardComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
