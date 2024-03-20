6d381d1a5389205574f0e5627c4b7ad86f193017
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 18:21:06 +0200

    Update text.ts

--- angular-canvas/src/app/classes/text.ts
+++ angular-canvas/src/app/classes/text.ts
@@ -4,14 +4,17 @@ export class TextFigure extends Figure {
     text: string;
     fontSize: number;
   
-    constructor(x: number, y: number, text: string, fontSize: number) {
-      super(x, y);
+    constructor(x: number, y: number, text: string, fontSize: number, backgroundColor: string, borderColor: string) {
+      super(x, y, backgroundColor, borderColor);
       this.text = text;
       this.fontSize = fontSize;
     }
   
     draw(ctx: CanvasRenderingContext2D): void {
+      ctx.fillStyle = this.backgroundColor;
+      ctx.strokeStyle = this.borderColor;
       ctx.font = this.fontSize + "px serif";
       ctx.fillText(this.text, this.x, this.y);
+      
     }
   }
