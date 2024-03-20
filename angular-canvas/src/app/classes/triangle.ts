ce8980a677bb35535631e1cff93b69ff49d42fe7
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 18:20:58 +0200

    Update triangle.ts

--- angular-canvas/src/app/classes/triangle.ts
+++ angular-canvas/src/app/classes/triangle.ts
@@ -4,20 +4,24 @@ export class Triangle extends Figure {
     endX: number;
     endY: number;
   
-    constructor(x: number, y: number, endX: number, endY: number) {
-      super(x, y);
+    constructor(x: number, y: number, endX: number, endY: number, backgroundColor: string, borderColor: string) {
+      super(x, y, backgroundColor, borderColor);
       this.endX = endX;
       this.endY = endY;
     }
   
     draw(ctx: CanvasRenderingContext2D): void {
-      ctx.strokeStyle = "green";
+      ctx.save();
+      ctx.fillStyle = this.backgroundColor;
+      ctx.strokeStyle = this.borderColor;
       ctx.beginPath();
       ctx.moveTo(this.x, this.y);
       ctx.lineTo(this.endX, this.endY);
       ctx.lineTo(this.x + (this.endX - this.x) / 2, this.y - (this.endY - this.y));
       ctx.lineTo(this.x, this.y);
+      ctx.fill();
       ctx.stroke();
       ctx.closePath();
+      ctx.restore();
     }
   }
