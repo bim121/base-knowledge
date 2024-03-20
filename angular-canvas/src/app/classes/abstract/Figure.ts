58f6be3708849bfee4741ad50e7f7b066425512a
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 18:22:07 +0200

    Update Figure.ts

--- angular-canvas/src/app/classes/abstract/Figure.ts
+++ angular-canvas/src/app/classes/abstract/Figure.ts
@@ -1,10 +1,14 @@
 export abstract class Figure {
     x: number;
     y: number;
+    backgroundColor: string;
+    borderColor: string;
   
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, backgroundColor: string, borderColor: string) {
       this.x = x;
       this.y = y;
+      this.backgroundColor = backgroundColor;
+      this.borderColor = borderColor;
     }
   
     abstract draw(ctx: CanvasRenderingContext2D): void;
