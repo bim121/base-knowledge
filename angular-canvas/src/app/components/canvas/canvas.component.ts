5bf69247823538c93bf2669df5008b87c4b387fe
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 12:46:38 +0200

    Update canvas.component.ts

--- angular-canvas/src/app/components/canvas/canvas.component.ts
+++ angular-canvas/src/app/components/canvas/canvas.component.ts
@@ -15,6 +15,7 @@ export class CanvasComponent {
   @ViewChild('canvas') public canvasRef!: ElementRef<HTMLCanvasElement>;
   public canvas!: HTMLCanvasElement;
   public ctx!: CanvasRenderingContext2D;
+  private image: HTMLImageElement = new Image();
   public isResizing = false;
   public startX = 0;
   public startY = 0;
@@ -28,6 +29,10 @@ export class CanvasComponent {
   ngAfterViewInit(): void {
     this.canvas = this.canvasRef.nativeElement;
     this.ctx = this.canvas.getContext('2d')!;
+    this.image.src = "../../../assets/image/home.svg";
+    this.image.onload = () => {
+        this._figureService.clearAndDraw(this.canvas, this.ctx);
+    };
     this.initCanvas();
   }
 
@@ -62,9 +67,33 @@ export class CanvasComponent {
 
     if(this.isResizing && this.selectedFigure !== null){
         if (this.selectedFigure === "rectangle") {
-            const squareSize = Math.abs(currentX - this.startX);
-            this._figureService.clearAndDraw(this.canvas, this.ctx);
-            this._figureService.drawRectangle(this.startX, this.startY, squareSize, this.ctx);
+          const squareSize = Math.abs(currentX - this.startX);
+          this._figureService.clearAndDraw(this.canvas, this.ctx);
+          this._figureService.drawRectangle(this.startX, this.startY, squareSize, this.ctx);
+        }
+        if (this.selectedFigure === "triangle") {
+          this._figureService.clearAndDraw(this.canvas, this.ctx);
+          this._figureService.drawTriangle(this.startX, this.startY, currentX, currentY, this.ctx);
+        }
+        if (this.selectedFigure === "horizontal") {
+          this._figureService.clearAndDraw(this.canvas, this.ctx);
+          this._figureService.drawLine(this.startX, this.startY, currentX, currentY, this.ctx);
+        }
+        if (this.selectedFigure === "ellipse") {
+          const radiusX = Math.abs(currentX - this.startX);
+          const radiusY = Math.abs(currentY - this.startY);
+          this._figureService.clearAndDraw(this.canvas, this.ctx);
+          this._figureService.drawEllipse(this.startX, this.startY, radiusX, radiusY, this.ctx);
+        }
+        if(this.selectedFigure === "text") {
+          const fontSize = Math.abs(currentX - this.startX); 
+          this._figureService.clearAndDraw(this.canvas, this.ctx);
+          this._figureService.drawText(this.startX, this.startY,"text", fontSize, this.ctx);
+        }
+        if (this.selectedFigure === "image") {
+          const imageSize = Math.abs(currentX - this.startX);
+          this._figureService.clearAndDraw(this.canvas, this.ctx);
+          this._figureService.drawImage(this.startX, this.startY, imageSize, this.image, this.ctx);
         }
     }
   }
@@ -76,9 +105,28 @@ export class CanvasComponent {
         const endX = event.clientX - rect.left;
         const endY = event.clientY - rect.top;
 
-        if (this.selectedFigure=== "rectangle") {
-            const squareSize = Math.abs(endX - this.startX); 
-            this._figureService.addRectangle(this.startX, this.startY, squareSize);
+        if (this.selectedFigure === "rectangle") {
+          const squareSize = Math.abs(endX - this.startX); 
+          this._figureService.addRectangle(this.startX, this.startY, squareSize);
+        }
+        if (this.selectedFigure === "triangle") {
+          this._figureService.addTriangle(this.startX, this.startY, endX, endY);
+        }
+        if (this.selectedFigure === "horizontal") {
+          this._figureService.addLine(this.startX, this.startY, endX, endY);
+        }
+        if (this.selectedFigure === "ellipse") {
+          const radiusX = Math.abs(endX - this.startX);
+          const radiusY = Math.abs(endY - this.startY);
+          this._figureService.addEllipse(this.startX, this.startY, radiusX, radiusY);
+        }
+        if (this.selectedFigure === "text") {
+          const fontSize = Math.abs(endX - this.startX);
+          this._figureService.addText(this.startX, this.startY, "text", fontSize);
+        }
+        if (this.selectedFigure === "image") {
+          const imageSize = Math.abs(endX - this.startX);
+          this._figureService.addImage(this.startX, this.startY, imageSize, this.image);
         }
         this._figureService.clearAndDraw(this.canvas, this.ctx);
     }
