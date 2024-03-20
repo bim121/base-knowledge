00c63fa3b857136160c64983c070c5078ac55be5
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 18:17:02 +0200

    Create option.interface.ts

---
+++ angular-canvas/src/app/shared/interface/option.interface.ts
@@ -0,0 +1,10 @@
+export interface Options {
+    backgroundColor: string;
+    borderColor: string;
+    width: number;
+    height: number;
+    src: string;
+    rotation: number;
+    text: string;
+    textColor: string;
+}
