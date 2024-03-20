c1426551bd08335887f116d2255cb895c3c53b50
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 18:17:15 +0200

    Update app.module.ts

--- angular-canvas/src/app/app.module.ts
+++ angular-canvas/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SelectableDirective } from './shared/directives/SelectableDirective.directive';
 import { CanvasComponent } from './components/canvas/canvas.component';
+import { OptionsComponent } from './components/options/options.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,8 @@ import { CanvasComponent } from './components/canvas/canvas.component';
     DashboardComponent,
     SelectableDirective,
     NavbarComponent,
-    CanvasComponent
+    CanvasComponent,
+    OptionsComponent,
   ],
   imports: [
     ReactiveFormsModule,
@@ -27,7 +30,9 @@ import { CanvasComponent } from './components/canvas/canvas.component';
     FormsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
