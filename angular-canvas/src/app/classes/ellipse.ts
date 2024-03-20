a275f94a7e4d2dbf3d1a667877ed32e7a98cc177
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 18:21:58 +0200

    Update ellipse.ts

--- angular-canvas/src/app/classes/ellipse.ts
+++ angular-canvas/src/app/classes/ellipse.ts
@@ -4,16 +4,18 @@ export class Ellipse extends Figure {
   radiusX: number;
   radiusY: number;
 
-  constructor(x: number, y: number, radiusX: number, radiusY: number) {
-    super(x, y);
+  constructor(x: number, y: number, radiusX: number, radiusY: number, backgroundColor: string, borderColor: string) {
+    super(x, y, backgroundColor, borderColor);
     this.radiusX = radiusX;
     this.radiusY = radiusY;
   }
 
   draw(ctx: CanvasRenderingContext2D): void {
-    ctx.strokeStyle = "red";
+    ctx.strokeStyle = this.borderColor;
+    ctx.fillStyle = this.backgroundColor;
     ctx.beginPath();
     ctx.ellipse(this.x, this.y, this.radiusX, this.radiusY, 0, 0, 2 * Math.PI);
+    ctx.fill();
     ctx.stroke();
     ctx.closePath();
   }
