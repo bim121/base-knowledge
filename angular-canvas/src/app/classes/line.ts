63209abdf4694bff6e083467042473471113b53a
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 18:21:42 +0200

    Update line.ts

--- angular-canvas/src/app/classes/line.ts
+++ angular-canvas/src/app/classes/line.ts
@@ -4,14 +4,14 @@ export class Line extends Figure {
     endX: number;
     endY: number;
   
-    constructor(x: number, y: number, endX: number, endY: number) {
-      super(x, y);
+    constructor(x: number, y: number, endX: number, endY: number, backgroundColor: string, borderColor: string) {
+      super(x, y, backgroundColor, borderColor);
       this.endX = endX;
       this.endY = endY;
     }
   
     draw(ctx: CanvasRenderingContext2D): void {
-        ctx.strokeStyle = "orange";
+        ctx.strokeStyle = this.backgroundColor;
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(this.endX, this.endY);
