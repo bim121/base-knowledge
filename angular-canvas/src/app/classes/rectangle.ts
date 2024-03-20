94af0af95c142ea9f02b886a9a44243934905c0c
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-20 18:21:17 +0200

    Update rectangle.ts

--- angular-canvas/src/app/classes/rectangle.ts
+++ angular-canvas/src/app/classes/rectangle.ts
@@ -3,13 +3,15 @@ import { Figure } from "./abstract/Figure";
 export class Rectangle extends Figure {
     size: number;
   
-    constructor(x: number, y: number, size: number) {
-      super(x, y);
+    constructor(x: number, y: number, size: number, backgroundColor: string, borderColor: string) {
+      super(x, y, backgroundColor, borderColor);
       this.size = size;
     }
   
     draw(ctx: CanvasRenderingContext2D): void {
-      ctx.strokeStyle = "blue";
-      ctx.strokeRect(this.x, this.y, this.size, this.size);
+      ctx.strokeStyle = this.borderColor;
+      ctx.fillStyle = this.backgroundColor;
+      ctx.fillRect(this.x, this.y, this.size, this.size);
+      ctx.strokeRect(this.x, this.y, this.size, this.size)
     }
 }
