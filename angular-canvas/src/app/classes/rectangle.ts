cf0ad919f7f91345cfa76c322e0a86f68ba4b03a
bim121 <118322970+bim121@users.noreply.github.com>
2024-03-21 18:25:21 +0200

    Update rectangle.ts

--- angular-canvas/src/app/classes/rectangle.ts
+++ angular-canvas/src/app/classes/rectangle.ts
@@ -1,17 +1,25 @@
 import { Figure } from "./abstract/Figure";
 
 export class Rectangle extends Figure {
-    size: number;
+  size: number;
   
-    constructor(x: number, y: number, size: number, backgroundColor: string, borderColor: string) {
-      super(x, y, backgroundColor, borderColor);
-      this.size = size;
-    }
+  constructor(x: number, y: number, size: number, backgroundColor: string, borderColor: string) {
+    super(x, y, backgroundColor, borderColor);
+    this.size = size;
+  }
   
-    draw(ctx: CanvasRenderingContext2D): void {
-      ctx.strokeStyle = this.borderColor;
-      ctx.fillStyle = this.backgroundColor;
-      ctx.fillRect(this.x, this.y, this.size, this.size);
-      ctx.strokeRect(this.x, this.y, this.size, this.size)
-    }
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.strokeStyle = this.borderColor;
+    ctx.fillStyle = this.backgroundColor;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+    ctx.strokeRect(this.x, this.y, this.size, this.size)
+  }
+
+  drawBorder(ctx: CanvasRenderingContext2D): void {
+    ctx.strokeRect(this.x, this.y, this.size, this.size);
+  }
+
+  isPointInside(x: number, y: number): boolean {
+    return x >= this.x && x <= this.x + this.size && y >= this.y && y <= this.y + this.size;
+  }
 }
